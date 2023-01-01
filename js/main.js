12605a35eacd8f8038fd6694ebceff1871145932
sirvan mirzapour <sirvanmirzapour@hotmail.com>
2023-01-01 20:33:04 +0330

    add shop ...

--- js/main.js
+++ js/main.js
@@ -206,7 +206,6 @@ $(document).ready(function() {
 
     $(".brand .owl-carousel").owlCarousel({
         rtl: true,
-        loop: true,
         items: 7,
         nav: true,
         dots: false,
@@ -214,7 +213,37 @@ $(document).ready(function() {
 
         responsive: {
             0: {
-                items: 1,
+                items: 2,
+
+            },
+            300: {
+                items: 2,
+
+            },
+            600: {
+                items: 4,
+
+            },
+            1000: {
+                items: 7,
+
+            }
+        }
+
+    });
+
+    //////////////////////////////////////////////// seen ////////////////////////////////////////////////
+
+    $(".lastseen .owl-carousel").owlCarousel({
+        rtl: true,
+        items: 7,
+        nav: true,
+        dots: false,
+        navText: ["<i class='fal fa-chevron-right'></i>", "<i class='fal fa-chevron-left'></i>"],
+
+        responsive: {
+            0: {
+                items: 2,
 
             },
             300: {
